ca521ce2b059b7e0471113dd07434a23f642c5e3
atul <atulk19966@gmail.com>
2019-09-26 09:54:15 +0530

    First commit

---
+++ src/main/java/com/stackroute/AppConfig.java
@@ -0,0 +1,28 @@
+package com.stackroute;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public actor getactor()
+    {
+        actor actor = new actor();
+        actor.setName("Akshay kumar");
+        actor.setGender("Male");
+        actor.setAge(70);
+        return actor;
+
+    }
+
+
+    @Bean
+    public  movie getmovie()
+    {
+        movie movie = new movie();
+        return movie;
+    }
+}
