d1f1a650b3388a93413faca0a37b89afd0ac3672
atul <atulk19966@gmail.com>
2019-09-26 15:53:08 +0530

    third commit

--- src/main/java/com/stackroute/AppConfig.java
+++ src/main/java/com/stackroute/AppConfig.java
@@ -1,28 +1,37 @@
 package com.stackroute;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public actor getactor()
+    public actor getactor1()
     {
-        actor actor = new actor();
-        actor.setName("Akshay kumar");
-        actor.setGender("Male");
-        actor.setAge(70);
-        return actor;
-
+        actor actor1 = new actor("Amitabh ","Male",72);
+        return actor1;
+    }
+    @Bean
+    public actor getactor2()
+    {
+        actor actor2 = new actor("Akshay","Male",55);
+        return actor2;
     }
 
-
-    @Bean
-    public  movie getmovie()
+    @Bean(value = "movie1")
+    public movie getmovie1()
     {
-        movie movie = new movie();
-        return movie;
+        movie movie1 = new movie(getactor1());
+        return movie1;
+    }
+
+    @Bean(value = "movie2")
+    public movie getmovie2()
+    {
+        movie movie2 = new movie(getactor2());
+        return movie2;
     }
 }
